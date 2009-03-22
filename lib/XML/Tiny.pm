0b8ea30c48180b6fd3097a1837bd2d03aa7e033d
David Cantrell <david@cantrell.org.uk>
2009-03-22 19:24:19 +0000

    Version 2.0, adding parsedommish()
    Add a vaguely DOM-like interface

--- lib/XML/Tiny.pm
+++ lib/XML/Tiny.pm
@@ -7,7 +7,7 @@ require Exporter;
 use vars qw($VERSION @EXPORT_OK @ISA);
 
 $VERSION = '1.12';
-@EXPORT_OK = qw(parsefile);
+@EXPORT_OK = qw(parsefile parsedommish);
 @ISA = qw(Exporter);
 
 # localising prevents the warningness leaking out of this module
@@ -152,6 +152,71 @@ A scalar piece of text.
 
 =back
 
+=head2 parsedommish
+
+This takes exactly the same parameters as C<parsefile()> but instead
+returns a XML::Tiny::DOMmish::Node object that you can query in a
+sort-of DOMmish way.
+
+It provides the following methods:
+
+=head3 various created by AUTOLOAD
+
+Given this XML:
+
+    <rsnapshot>
+      <snapshotroot nocreateroot='1'>
+        /.snapshots/
+      </snapshotroot>
+      <externalprograms>
+        <rsync binary='/usr/bin/rsync'>
+          <shortargs>
+            <arg>-a</arg>
+            <arg>-q</arg>
+          </shortargs>
+          <longargs>
+            <arg>--delete</arg>
+          </longargs>
+        </rsync>
+      </externalprograms>
+    </rsnapshot>
+
+then you can query it thus:
+
+    my $rsync = $document->externalprograms->rsync();
+    my $rsyncbinary = $rsync->binary();
+    my $rsyncargs   = [
+        $rsync->shortargs->values(),
+        $srync->longargs->values()
+    ];
+    my $secondshortarg = $config->rsync->shortargs->arg(1);
+    my @shortargs      = $config->rsync->shortargs->arg('*');
+
+The methods (eg C<binary>, C<rsync> etc) in the synopsis above are created
+using AUTOLOAD.  The AUTOLOADer first looks for an attribute with the
+appropriate name and if it exists, returns its contents.
+
+If no such attribute is found, then it looks for a child node of the
+appropriate type.  If no parameter is given, it returns the first one.
+
+If a numeric parameter is given it returns the Nth such node - they are
+numbered from 0.
+
+If the parameter is an asterisk (C<*>) then a list of all such nodes
+is returned.
+
+Otherwise the node of the appropriate type whose C<-E<gt>name()> method
+matches is returned.  It is an error to try this if there's no such
+method.
+
+Nodes stringify to their contents if necessary, and can also be compared
+for string (in)equality.  Note that when stringifying, leading and
+trailing whitespace is removed.
+
+=head3 values
+
+Returns a list of the string contents of all child nodes.
+
 =cut
 
 my %regexps = (
@@ -160,6 +225,10 @@ my %regexps = (
 
 my $strict_entity_parsing; # mmm, global. don't worry, parsefile sets it
                            # explicitly every time
+sub parsedommish {
+    return XML::Tiny::DOMmish::Node->new(parsefile(@_)->[0]);
+}
+
 sub parsefile {
     my($arg, %params) = @_;
     my($file, $elem) = ('', { content => [] });
@@ -425,10 +494,6 @@ any necessary XML data, which demonstrates the bug.  Ideally, this
 will be in the form of a file which I can drop in to the module's
 test suite.  Please note that such files must work in perl 5.004.
 
-If you are feeling particularly generous you can encourage me in my
-open source endeavours by buying me something from my wishlist:
-  L<http://www.cantrell.org.uk/david/wishlist/>
-
 =head1 SEE ALSO
 
 =over 4
@@ -445,7 +510,7 @@ L<http://beta.nntp.perl.org/group/perl.datetime/2007/01/msg6584.html>
 
 =back
 
-=head1 AUTHOR
+=head1 AUTHOR, COPYRIGHT and LICENCE
 
 David Cantrell E<lt>F<david@cantrell.org.uk>E<gt>
 
@@ -463,17 +528,98 @@ and for reporting another bug that I introduced when fixing the first one;
 
 to 'Corion' for finding a bug with localised filehandles and providing a fix.
 
-=head1 COPYRIGHT and LICENCE
+Copyright 2007-2009 David Cantrell E<lt>david@cantrell.org.ukE<gt>
 
-Copyright 2007 David Cantrell
-
-This module is free-as-in-speech software, and may be used, distributed,
-and modified under the same terms as Perl itself.
+his software is free-as-in-speech software, and may be used,
+distributed, and modified under the terms of either the GNU
+General Public Licence version 2 or the Artistic Licence.  It's
+up to you which one you use.  The full text of the licences can
+be found in the files GPL2.txt and ARTISTIC.txt, respectively.
 
 =head1 CONSPIRACY
 
 This module is also free-as-in-mason software.
 
 =cut
+
+package XML::Tiny::DOMmish::Node;
+
+use vars qw($AUTOLOAD);
+use overload
+    '""'  => sub { return shift()->_gettext(); },
+    'eq'  => sub { my $s = shift(); return $s->_compare('eq', @_) },
+    'ne'  => sub { my $s = shift(); return $s->_compare('ne', @_) },
+    ;
+sub new {
+    my $class = shift;
+    my $document = shift;
+    bless $document, $class;
+}
+
+sub AUTOLOAD {
+    (my $nodename = $AUTOLOAD) =~ s/.*:://;
+    my $self   = shift();
+    my $wanted = shift() || 0;
+
+    # attribs take precedence ...
+    return $self->{attrib}->{$nodename}
+        if(exists($self->{attrib}->{$nodename}));
+
+    my @childnodes = ();
+    foreach my $childnode (@{$self->{content}}) {
+        if($childnode->{type} eq 'e' && $childnode->{name} eq $nodename) {
+            push @childnodes, __PACKAGE__->new($childnode);
+        }
+    }
+    if($wanted eq '*') {
+        return @childnodes;
+    } elsif($wanted =~ /^\d+$/) {
+        return $childnodes[$wanted] if(exists($childnodes[$wanted]));
+        die("Can't get '$nodename' number $wanted from object ".ref($self)."\n")
+;
+    } else {
+        return (grep { $_->name() eq $wanted } @childnodes)[0];
+    }
+}
+sub values {
+    my $self = shift;
+    my @values = ();
+    if(exists($self->{content}) && ref($self->{content}) eq 'ARRAY') {
+        push @values, __PACKAGE__->new($_)->_gettext()
+            foreach (@{$self->{content}});
+    }
+    return @values;
+}
+
+sub _gettext {
+    my $self = shift;
+    my $c = $self->{content};
+    if(
+        ref($c) eq 'ARRAY' && # there's some contents
+        $c->[0]->{type} eq 't' # it's a text node
+    ) {
+        (my $value = $c->[0]->{content}) =~ s/^\s+|\s+$//g;
+        return $value;
+    } else {
+        die("Can't stringify '".$self->{name}."' in ".ref($self)."\n");
+    }
+}
+
+sub _compare {
+    my($self, $op, $comparand, $reversed) = @_;
+    my $value = $self->_gettext();
+    ($value, $comparand) = ($comparand, $value) if($reversed);
+    return ($op eq 'eq') ? $value eq $comparand :
+           ($op eq 'ne') ? $value ne $comparand :
+           # ($op eq 'lt') ? $value lt $comparand :
+           # ($op eq 'le') ? $value le $comparand :
+           # ($op eq 'gt') ? $value gt $comparand :
+           # ($op eq 'ge') ? $value ge $comparand :
+           # ($op eq 'cmp') ? $value cmp $comparand :
+           die("_compare can't $op\n");
+}
+sub DESTROY {}
+
+
 
 '<one>zero</one>';
